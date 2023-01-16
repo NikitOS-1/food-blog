8a3613f52924f8ae4d2f1e00f333684586faeea7
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-01-17 01:17:05 +0200

    complead Footer

--- src/container/Footer/Footer.tsx
+++ src/container/Footer/Footer.tsx
@@ -1,7 +1,27 @@
 import style from "./Footer.module.scss";
+import iconFacebook from "../../assets/icon-facebook.webp";
+import iconTwiter from "../../assets/icon-twiter.webp";
+import iconInstagram from "../../assets/icon-instagram.webp";
 
 type Props = {};
 const Footer = (props: Props) => {
-  return <div>Footer</div>;
+  return (
+    <footer className={style.footer}>
+      <div className={style.links}>
+        <a href="https://facebook.com">
+          <img src={iconFacebook} alt="facebook" />
+        </a>
+        <a href="https://twitter.com/?lang=uk">
+          <img src={iconTwiter} alt="twitter" />
+        </a>
+        <a href="https://www.instagram.com/">
+          <img src={iconInstagram} alt="instagram" />
+        </a>
+      </div>
+      <div className={style.after}>
+        © 2023 Еда как искусство. Nikita Puzyrenko
+      </div>
+    </footer>
+  );
 };
 export default Footer;
