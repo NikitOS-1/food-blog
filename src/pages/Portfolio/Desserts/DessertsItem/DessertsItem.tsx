350034538c306b3f098f4b317fc536035cd358aa
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-01-27 13:15:33 +0200

    Refact style

--- src/pages/Portfolio/Desserts/DessertsItem/DessertsItem.tsx
+++ src/pages/Portfolio/Desserts/DessertsItem/DessertsItem.tsx
@@ -1,3 +1,5 @@
+import style from "./DessertsItem.module.scss";
+
 type Props = {
   id: number;
   category: string;
@@ -8,14 +10,16 @@ type Props = {
 
 const DessertsItem = ({ id, category, image, title, description }: Props) => {
   return (
-    <>
-      <div>id: {id}</div>
-      <div>category: {category}</div>
-      <div>image: {image}</div>
-      <img src={image} alt="art" />
-      <div>title: {title}</div>
-      <div>description: {description}</div>
-    </>
+    <div className={style.wrap}>
+      <div className={style.item}>
+        <div className={style.image}>
+          <img src={image} alt="art" />
+        </div>
+        <div className={style.title}>{title}</div>
+        <div className={style.description}>{description}</div>
+        <div className={style.itemOption}></div>
+      </div>
+    </div>
   );
 };
 export default DessertsItem;
