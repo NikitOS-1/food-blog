29f611f14abbf3fc5bb5800584873734bdb4df88
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-02-01 15:39:32 +0200

    add arr coments and state

--- src/componets/ArticalItem/Coment/Coment.tsx
+++ src/componets/ArticalItem/Coment/Coment.tsx
@@ -1,9 +1,57 @@
+import { useState } from "react";
 import style from "./Coment.module.scss";
 
 type Props = {};
 
+type Coment = {
+  name: string;
+  text: string;
+};
+
 const Coment = (props: Props) => {
-  return <div className={style.wrap}>Coment</div>;
+  let arrComents: Coment[] = [
+    {
+      name: "nik",
+      text: "hello",
+    },
+    {
+      name: "sasha",
+      text: "hello",
+    },
+    {
+      name: "sasha",
+      text: "hello",
+    },
+  ];
+  const [coment, setComent] = useState<Coment[]>(arrComents);
+  const [newComent, setNewComent] = useState<Coment>({
+    name: "",
+    text: "",
+  });
+
+  return (
+    <div className={style.wrap}>
+      <form>
+        <div>
+          <input type="text" />
+        </div>
+        <div>
+          <textarea></textarea>
+        </div>
+        <div>
+          <button>Send</button>
+        </div>
+      </form>
+      <div className={style.view}>
+        {coment.map(({ name, text }: Coment) => (
+          <div>
+            <div>{name}</div>
+            <div>{text}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Coment;
