eacca64b83bf65641fea9efd15d99602f60fe4e2
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-02-02 12:02:23 +0200

    compleated style for form(coment)

--- src/componets/ArticalItem/Coment/Coment.tsx
+++ src/componets/ArticalItem/Coment/Coment.tsx
@@ -46,21 +46,40 @@ const Coment = (props: Props) => {
   return (
     <div className={style.wrap}>
       <form className={style.form} onSubmit={handleSubmit}>
-        <div className={style.name}>
-          <input type="text" value={newComent.name} onChange={handleName} />
-        </div>
-        <div className={style.text}>
-          <textarea value={newComent.text} onChange={handleText}></textarea>
+        <div className={style.coment}>
+          <div className={style.name}>
+            <input
+              type="text"
+              placeholder="Enter your name ..."
+              value={newComent.name}
+              onChange={handleName}
+            />
+          </div>
+          <div className={style.text}>
+            <textarea
+              value={newComent.text}
+              onChange={handleText}
+              placeholder="Enter your comment ..."></textarea>
+          </div>
         </div>
         <div className={style.btnSend}>
-          <button type="submit">Send</button>
+          <button type="submit">
+            <svg width="32" height="32" viewBox="0 0 22 22" fill="none">
+              <path
+                d="M13.0137 10.7353L5.64685 10.7598L3.99419 3.81634C3.77771 2.90644 4.7472 2.16525 5.56581 2.61556L18.6662 9.8104C19.4102 10.2205 19.4028 11.295 18.6502 11.702L5.50222 18.8283C4.67928 19.2741 3.72369 18.5382 3.94828 17.6299L5.64685 10.7598"
+                stroke="#333333"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"></path>
+            </svg>
+          </button>
         </div>
       </form>
       <div className={style.view}>
         {coment.map(({ name, text }: Coment, i) => (
           <div key={i}>
-            <div>{name}</div>
-            <div>{text}</div>
+            <div className={style.newName}>{name}</div>
+            <div className={style.newText}>{text}</div>
           </div>
         ))}
       </div>
