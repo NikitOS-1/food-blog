f6e432e96cfc7d56dac4c72bd39278e0486fb667
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-02-07 12:03:11 +0200

    add react-icons and carousele

---
+++ src/componets/Carousele/Carousele.tsx
@@ -0,0 +1,10 @@
+import style from "./Carousele.module.scss";
+import { Carousel } from "@sefailyasoz/react-carousel";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+
+type Props = {};
+
+const Carousele = (props: Props) => {
+  return <div>Carousele</div>;
+};
+export default Carousele;
