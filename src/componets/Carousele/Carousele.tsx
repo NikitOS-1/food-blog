2b03e47535cd0002d73031fe804f0e77bd5e0731
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-02-07 12:31:13 +0200

    add caurosel to home

--- src/componets/Carousele/Carousele.tsx
+++ src/componets/Carousele/Carousele.tsx
@@ -1,8 +1,19 @@
+import Carousel from "nuka-carousel";
 import style from "./Carousele.module.scss";
 
 type Props = {};
 
 const Carousele = (props: Props) => {
-  return <div className={style.carousele}></div>;
+  return (
+    <div className={style.carousele}>
+      <Carousel wrapAround={true} slidesToShow={2}>
+        <img src="/images/steak/steak-6.webp" />
+        <img src="/images/steak/steak-7.webp" />
+        <img src="/images/steak/steak-8.webp" />
+        <img src="/images/steak/steak-3.webp" />
+        <img src="/images/steak/steak-2.webp" />
+      </Carousel>
+    </div>
+  );
 };
 export default Carousele;
