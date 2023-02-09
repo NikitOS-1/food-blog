bc193abae12125efcbbf8f8fdf8f2b5b6062db5e
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-02-09 11:49:23 +0200

    refactoring style in components

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -7,16 +7,22 @@ const Portfolio = (props: Props) => {
   return (
     <div className={style.portfolio}>
       <div className={style.meat}>
-        <h1>Meat</h1>
-        <ButtonAll to="/articles/meat">See more</ButtonAll>
+        <div className={style.wrap}>
+          <h1>Meat</h1>
+          <ButtonAll to="/articles/meat">See more</ButtonAll>
+        </div>
       </div>
       <div className={style.fish}>
-        <h1>Fish</h1>
-        <ButtonAll to="/articles/fish">See more</ButtonAll>
+        <div className={style.wrap}>
+          <h1>Fish</h1>
+          <ButtonAll to="/articles/fish">See more</ButtonAll>
+        </div>
       </div>
-      <div className={style.dishes}>
-        <h1>Desserts</h1>
-        <ButtonAll to="/articles/desserts">See more</ButtonAll>
+      <div className={style.desserts}>
+        <div className={style.wrap}>
+          <h1>Desserts</h1>
+          <ButtonAll to="/articles/desserts">See more</ButtonAll>
+        </div>
       </div>
     </div>
   );
