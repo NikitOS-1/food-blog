723c0b72ceeba19fb5b25d8774ff7534c7daf5f3
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-02-09 16:31:32 +0200

    FINALY REFACT

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -3,12 +3,7 @@ import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
 import "./App.scss";
 import { CssBaseline } from "@mui/material";
-
 import articalsArray from "../../state/articalsArray";
-
-type FavoriteArticle = {
-  [id: number]: number;
-};
 
 const App = () => {
   return (
